1ad9ae83a9506e510648ec6312a1ce39ec405f6e
Conrad M Albrecht <cmalbrec@us.ibm.com>
2021-02-22 15:10:11 -0500

    update copyright info II
    DCO 1.1 Signed-off-by: Conrad M Albrecht <cmalbrec@us.ibm.com>

--- ibmpairs/logger.py
+++ ibmpairs/logger.py
@@ -2,7 +2,7 @@
 IBM PAIRS RESTful API wrapper: A Python module to access PAIRS's core API to
 load data into Python compatible data formats.
 
-Copyright 2019-2020 Physical Analytics, IBM Research All Rights Reserved.
+Copyright 2019-2021 Physical Analytics, IBM Research All Rights Reserved.
 
 SPDX-License-Identifier: BSD-3-Clause
 """
